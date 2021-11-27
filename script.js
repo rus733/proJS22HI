7803759dda33248a661938c9ca26c277ac00a83f
rus733 <9282716617@mail.ru>
2021-11-27 10:52:26 +0300

    исправлены замечания 7 шт

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let num = 266219;
-let a;
+const num = 266219;
 
-const getMultiple = (num) =>
-  num
-    .toString()
-    .split("")
-    .reduce((res, item) => item * res, 1);
+//const getMultiple = (num) =>
+//num
+//.toString()
+//.split("")
+//.reduce((res, item) => item * res, 1);
 
-console.log(getMultiple(num));
+let ansMult = num
+  .toString()
+  .split("")
+  .reduce((res, item) => item * res);
+
+console.log(ansMult);
 //a = getMultiple(num);
 //a = a ** 3;
 //console.log(a);
 
-a = getMultiple(num) ** 3;
-
+//a = getMultiple(num) ** 3;
+ansMult = ansMult ** 3;
 //a = a.toString().slice(0, 2);
-console.log(a.toString().slice(0, 2));
+console.log(ansMult.toString().slice(0, 2));
