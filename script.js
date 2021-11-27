ea8ace3bff42f153dc7a01cdc08155bc5362cf0f
rus733 <9282716617@mail.ru>
2021-11-27 10:56:48 +0300

    убрал коментированный код

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const num = 266219;
 
-//const getMultiple = (num) =>
-//num
-//.toString()
-//.split("")
-//.reduce((res, item) => item * res, 1);
-
 let ansMult = num
   .toString()
   .split("")
   .reduce((res, item) => item * res);
 
 console.log(ansMult);
-//a = getMultiple(num);
-//a = a ** 3;
-//console.log(a);
 
-//a = getMultiple(num) ** 3;
 ansMult = ansMult ** 3;
-//a = a.toString().slice(0, 2);
+
 console.log(ansMult.toString().slice(0, 2));
