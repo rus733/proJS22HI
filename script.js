4c3934f7ddc6ab7fad1ce3375b6eb2ea2cb931c3
rus733 <9282716617@mail.ru>
2021-11-29 19:22:54 +0300

    готовим место для lesson03Hi

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-//репозиторий для усложненных задач
-const num = 266219;
-
-let ansMult = num
-  .toString()
-  .split("")
-  .reduce((res, item) => item * res);
-
-console.log(ansMult);
-ansMult = ansMult ** 3;
-console.log(ansMult.toString().slice(0, 2));
+"use strict";
