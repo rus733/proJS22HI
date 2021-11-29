7aae86c9137c064117348adef95f84f23f5953a2
rus733 <9282716617@mail.ru>
2021-11-29 19:24:02 +0300

    create temp.js for old code

---
+++ temp.js
@@ -0,0 +1,10 @@
+const num = 266219;
+
+let ansMult = num
+  .toString()
+  .split("")
+  .reduce((res, item) => item * res);
+
+console.log(ansMult);
+ansMult = ansMult ** 3;
+console.log(ansMult.toString().slice(0, 2));
