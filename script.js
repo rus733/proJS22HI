5f6165f1f560a8559b55ced7490c26df69ee65f3
rus733 <9282716617@mail.ru>
2021-11-29 20:13:36 +0300

    create first code

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const getDayName = (number, lang) =>
+  ({
+    en: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    ru: [
+      "Воскресенье",
+      "Понедельник",
+      "Вторник",
+      "Среда",
+      "Четверг",
+      "Пятница",
+      "Суббота",
+    ],
+  }[lang][number % 7]);
+
+console.log(getDayName(5, "en")); // Friday
+console.log(getDayName(7, "ru")); // Воскресенье
