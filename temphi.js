36b7a61c59c0da0187e07e0589fa5f6e9fe8a415
rus733 <9282716617@mail.ru>
2021-11-30 21:30:55 +0300

    создал temphi.js и неренес в него предыдущий урок

---
+++ temphi.js
@@ -0,0 +1,35 @@
+"use strict";
+//Принято
+
+//готовим задание 4
+
+const lang = "en";
+
+const arrDayName = {
+  en: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  ru: ["Пн", "Вт", "СР", "Чт", "Пт", "Сб", "Вс"],
+};
+
+if (lang === "ru") {
+  console.log(arrDayName.ru);
+} else {
+  console.log(arrDayName.en);
+}
+
+switch (lang) {
+  case "ru":
+    console.log(arrDayName["ru"]);
+    break;
+  case "en":
+    console.log(arrDayName["en"]);
+    break;
+}
+
+console.log(arrDayName[lang]);
+
+const namePerson = "Артем";
+namePerson === "Артем"
+  ? console.log("директор")
+  : namePerson === "Александр"
+  ? console.log("преподаватель")
+  : console.log("студент");
