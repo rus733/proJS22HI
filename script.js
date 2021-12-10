edbc7981f041e8cac5a7ab095d3904c5618f8fdb
rus733 <9282716617@mail.ru>
2021-12-10 12:49:59 +0300

    переделал , назначил конст Button через Id, и вынес все дейст в функцию button.onclick  lesson10hi

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
 //  готовим задание 10hi
-
+const button = document.getElementById("button");
+console.dir(button);
+/*
 function createNewLi() {
   const inputText = document.querySelector('[type = "text"]');
   const newElementLi = document.createElement("li");
@@ -11,4 +13,13 @@ function createNewLi() {
   ulParent.append(newElementLi); // вставить newLi в конец <ul>
 }
 
-button.addEventListener("click", createNewLi);
+//button.addEventListener("click", createNewLi);
+button.onclick = createNewLi();
+*/
+button.onclick = function () {
+  const inputText = document.querySelector('[type = "text"]');
+  const newElementLi = document.createElement("li");
+  const ulParent = document.querySelector("ul");
+  newElementLi.innerHTML = inputText.value;
+  ulParent.append(newElementLi); // вставить newLi в конец <ul>
+};
