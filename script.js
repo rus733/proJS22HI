c93019f356dc2920ed1e3c53b92fdb2df3e0df82
rus733 <9282716617@mail.ru>
2021-12-13 11:57:30 +0300

    привел функции к упрощенной записи , убрал лишние переменные ,вывел  % при загрузке  в spanRange   lesson11

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btn = document.getElementById('btn');
 const eBtn = document.getElementById('e_btn');
 const inputText = document.querySelector('#text');
 const square = document.querySelector('#square');
-const span = document.getElementById('text-span');
 const spanRange = document.getElementById('range-span');
 const inputRange = document.querySelector('[type=range]');
 const circle = document.getElementById('circle');
@@ -15,34 +14,18 @@ const circle = document.getElementById('circle');
 
 let сolorSquare = '';
 
-const readInputText = (event) => {
-  сolorSquare = event.target.value;
-};
-
-const newColorSquare = function (event) {
-  square.style.backgroundColor = сolorSquare;
-};
+const readInputText = (event) => (сolorSquare = event.target.value);
+const newColorSquare = () => (square.style.backgroundColor = сolorSquare);
 
 inputText.addEventListener('input', readInputText);
 btn.addEventListener('click', newColorSquare);
-
-/*
-
-// задание 1 упростил немного 
-
-const newColorSquare = function (event) {
-
-  inputText.addEventListener('input', square.style.backgroundColor = (event) => event.target.value);
-};
-
-btn.addEventListener('click', newColorSquare);
-*/
 
 // задание 2
 eBtn.style.display = 'none';
 
 //задание 3
-const circleSize = function (event) {
+spanRange.textContent = '50%';
+const circleSize = () => {
   spanRange.textContent = inputRange.value + '%';
   circle.style.height = inputRange.value + '%';
   circle.style.width = inputRange.value + '%';
